7ca038c21ef2ef0ed918626cf7f0fcd625a80a74
Ali Chegini <ali.c.engineer@gmail.com>
2018-12-24 16:49:07 +0100

    initial commit

---
+++ ProximityReminders/DetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailViewController.swift
+//  ProximityReminders
+//
+//  Created by Ehsan on 24/12/2018.
+//  Copyright © 2018 Ali C. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UITableViewController {
+
+    var detail: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        
+        return cell
+    }
+    
+}
---
+++ ProximityReminders/MasterViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MasterViewController.swift
+//  ProximityReminders
+//
+//  Created by Ehsan on 24/12/2018.
+//  Copyright © 2018 Ali C. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        
+        return cell
+    }
+}
